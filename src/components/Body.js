1518f48b3dfabc55732da5be1c409e2698a64d41
Coderner <shreyashukla680@gmail.com>
2023-08-03 17:35:01 +0530

    Created a search bar to filter restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,42 @@
 import RestaurantCard from "./RestaurantCard";
 import {restrauntList} from "./config";
+import {useState} from "react";
+
+function filterData(searchText, restaurants){
+  const filterData= restaurants.filter((restaurant)=> restaurant.info.name.includes(searchText));
+  return filterData;
+}
 
 const Body = () =>{
+
+    const [restaurants,setRestaurants] = useState(restrauntList);
+    const [searchText,setSearchText] = useState("");
+    
     return(
-        <div className="restraunt-list">
-           { restrauntList.map((restraunt)=>{
-            return <RestaurantCard {...restraunt.info} key={restraunt.info.id}/>
-           })}
-        </div>
+        <>
+            <div className="search-container">
+                 <input 
+                  type="text" 
+                  className="search-input" 
+                  placeholder="Search" 
+                  value={searchText}
+                  onChange={(e)=>{
+                    setSearchText(e.target.value)
+                  }}
+                 />
+                 <button className="search-btn" onClick={()=>{
+                     const data= filterData(searchText,restaurants);
+                     setRestaurants(data);
+                 }}>
+                    Search</button>
+            </div>
+            <div className="restraunt-list">
+             { restaurants.map((restraunt)=>{
+                 return <RestaurantCard {...restraunt.info} key={restraunt.info.id}/>
+             })}
+            </div>
+
+        </>
     )
 }
 
