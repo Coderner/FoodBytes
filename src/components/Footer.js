b635434d5270f500586c30c3b5e0eb3c7f789a9c
Coderner <shreyashukla680@gmail.com>
2023-09-21 10:28:20 +0530

    added a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,14 @@
+import Logo from "../assets/img/Logo.png";
+
 const Footer = () =>{
     return(
-       <h1>Footer</h1>
+       <div className="flex h-28 border-y-2 justify-center py-8 mt-6">
+          <img className="h-10" alt="logo" src={Logo}/>
+          <p className="my-2 mr-1 font-semibold"> created by </p>
+          <a href="https://github.com/Coderner" target="_blank" className="my-2 font-semibold text-red-600">
+              Shreya Shukla
+          </a>
+       </div>
     )
 }
 
